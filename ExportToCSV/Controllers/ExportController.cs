365dd5117c192f8bce3fa9b3bd60b9d82da33cec
muruganad <murugan@4beginner.com>
2023-08-22 14:22:12 +0530

    initial commit

---
+++ ExportToCSV/Controllers/ExportController.cs
@@ -0,0 +1,19 @@
+﻿using Csv;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExportToCSV.Controllers
+{
+    public class ExportController : Controller
+    {
+        public IActionResult Index()
+        {
+            var myExport = new CsvExport();
+            // usage 
+            // myExport.AddRows<Country>(List, Ignore Array )
+            myExport.AddRows<Country>(new Country().GetAll(), new string[] { "Id", "NumericId" });
+            Byte[] file = myExport.ExportToBytes();
+            return File(file, "application/octet-stream", "Countries.csv");
+        }
+
+    }
+}
---
+++ ExportToCSV/Models/Country.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExportToCSV
+{
+    public partial class Country
+    {
+        public int Id { get; set; }
+        public int NumericId { get; set; }
+        public int Code { get; set; }
+        public string? Name { get; set; }
+    }
+    public partial class Country
+    {
+
+        public List<Country> GetAll() {
+            List<Country> _Countrys = new List<Country>()
+            {
+                new Country(){ Id = 1, NumericId = 1, Code = 1, Name = "United States Of America"  },
+                new Country(){ Id = 2, NumericId = 2, Code = 91, Name = "India"  },
+                new Country(){ Id = 3, NumericId = 3, Code = 86, Name = "China"  },
+            };            
+            return _Countrys;
+        }
+    }
+
+    }
